58d56bdc811108fb842eadb8e68a3b1b5b821b3d
Sumon Hossain <sumonengr.96@gmail.com>
2023-11-13 19:06:30 +0600

    init commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,38 @@
 import { useState } from "react";
-import "./App.css";
+import { CodeBlock, dracula } from "react-code-blocks";
 
 function App() {
+  const [isCopied, setIsCopied] = useState(false);
+
   const exampleCode = `function greet(name: string): string {
     return 'Hello, ' + name + '!';
   }`;
-
-  return (
-    <>
-      <h2>Your React Component</h2>
-      <CodeBox code={exampleCode} />
-    </>
-  );
-}
-
-interface CodeBoxProps {
-  code: string;
-}
-
-const CodeBox: React.FC<CodeBoxProps> = ({ code }) => {
-  const [isCopied, setIsCopied] = useState(false);
+  const language = "typescript";
 
   const handleCopyClick = () => {
-    navigator.clipboard.writeText(code);
+    navigator.clipboard.writeText(exampleCode);
     setIsCopied(true);
-
     // Reset the "Copied" state after 2 seconds
     setTimeout(() => {
       setIsCopied(false);
     }, 2000);
   };
+
   return (
-    <div className="code-box">
-      <button className="copy-button" onClick={handleCopyClick}>
-        {isCopied ? "Copied!" : "Copy"}
-      </button>
-      <pre>
-        <code>{code}</code>
-      </pre>
-    </div>
+    <>
+      <div className="code-blocks">
+        <button className="copy-button" onClick={handleCopyClick}>
+          {isCopied ? "Copied!" : "Copy"}
+        </button>
+        <CodeBlock
+          text={exampleCode}
+          language={language}
+          showLineNumbers={false}
+          theme={dracula}
+        />
+      </div>
+    </>
   );
-};
+}
 
 export default App;
